c89cdd50d0b4d0f80854caf0b85f0d8f27317500
y68dnx <yousifabdulsattar1q@gmail.com>
2026-06-02 10:06:00 +0300

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', event => {
-  console.log('Service Worker installed');
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
-});
